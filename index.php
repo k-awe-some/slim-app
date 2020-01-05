5005ee19bac21f345e2d2673ba20ee57f11b653c
kay <kaynguyen.dev@gmail.com>
2020-01-05 18:01:45 -0500

    wired up middlewares; tested POST request with file uploads

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ include 'bootstrap.php';
 use Chatter\Models\Message;
 use Chatter\Middlewares\Logging as ChatterLogging;
 use Chatter\Middlewares\Authentication as ChatterAuth;
+use Chatter\Middlewares\FileFilter;
+use Chatter\Middlewares\FileMove;
+use Chatter\Middlewares\ImageRemoveExif;
 
 $app = new \Slim\App();
 $app->add(new ChatterAuth());
@@ -24,12 +27,17 @@ $app->get('/messages', function ($request, $response, $args) {
     return $response->withStatus(200)->withJson($payload);
 });
 
+$filter = new FileFilter();
+$removeExif = new ImageRemoveExif();
+$move = new FileMove();
+
 $app->post('/messages', function ($request, $response, $args) {
     $_message = $request->getParsedBodyParam('message', '');
 
     $message = new Message();
     $message->body = $_message;
     $message->user_id = -1;
+    $message->image_url = $request->getAttribute('png_filename');
     $message->save();
 
     if ($message->id) {
@@ -39,7 +47,7 @@ $app->post('/messages', function ($request, $response, $args) {
     } else {
         return $response->withStatus(400);
     }
-});
+})->add($filter)->add($removeExif)->add($move);
 
 $app->delete('/messages/{message_id}', function ($request, $response, $args) {
     $message = Message::find($args['message_id']);
